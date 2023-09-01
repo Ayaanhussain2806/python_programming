8138d12e96c9e50fbb8fbb143dd00f02eb5a6232
Ayaanhussain2806 <142593144+Ayaanhussain2806@users.noreply.github.com>
2023-09-01 01:17:30 -0500

    Seconds to hours and minutes

---
+++ second to hours.py
@@ -0,0 +1,10 @@
+a=int(input("Enter the second"))
+sec=a%60
+minutes=a//60
+min=minutes%60
+hour=minutes//60
+
+print("Given no of seconds",a)
+print("hours",hour)
+print("minute",min)
+print("seconds",sec)
